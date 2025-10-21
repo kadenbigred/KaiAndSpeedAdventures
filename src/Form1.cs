66b7e586b45446a6539311a699d1578fe09e248a
kadenbigred <157423927+kadenbigred@users.noreply.github.com>
2025-10-21 14:40:15 -0400

    Add files via upload

---
+++ src/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private int gravityVal = 0;
+        int xmove = 10;
+        int xbound = Screen.PrimaryScreen.Bounds.Width;
+        int ybound = Screen.PrimaryScreen.Bounds.Height;
+        string audioPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets", "song.wav");
+        string imgPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets", "speed.png");
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Make window
+            this.StartPosition = FormStartPosition.Manual;
+            this.Location = new Point(xbound/2, ybound/2);
+            this.Height = 300;
+            this.Width = 300;
+            this.DoubleBuffered = true;
+
+            // Add image
+            PictureBox pb1 = new PictureBox();
+            pb1.ImageLocation = imgPath;
+            pb1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pb1.Size = new Size(300,260);
+            this.Controls.Add(pb1);
+
+            //Set window title
+            this.Text = "Kai and Speed Adventures";
+
+            //Pin window
+            this.TopMost = true;
+
+            //Play audio
+            SoundPlayer player = new SoundPlayer(audioPath);
+            player.PlayLooping();
+
+            //Make window bounce around screen
+            var moveTimer = new Timer();
+            moveTimer.Interval = 15;
+            moveTimer.Tick += (s, e) =>
+            {
+                gravity();
+            };
+            moveTimer.Start();
+        }
+
+        private void gravity()
+        {
+            gravityVal += 2;
+            int gravMax = 30;
+
+            //If it hits the bottom, bounce
+            if(this.Location.Y >= ybound - this.Height)
+            {
+                gravityVal = -gravityVal;
+            }
+
+            //Dont let it get above the max height
+            if (gravityVal >= gravMax)
+            {
+                gravityVal = gravMax;
+            }
+            else if (gravityVal <= -gravMax)
+            {
+                gravityVal = -gravMax;
+            }
+
+            //If it hits the sides of the screen, bounce the other way
+            if (this.Location.X + this.Width >= xbound)
+            {
+                xmove = -xmove;
+            }else if (this.Location.X <= 0)
+            {
+                xmove = -xmove;
+            }
+               
+            //Apply gravity to window location
+            this.Location = new Point(this.Location.X + xmove, this.Location.Y + gravityVal);
+        }
+
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows.Forms;
+using System;
+using WindowsFormsApp1;
+
+class MyContext : ApplicationContext
+{
+    
+    public MyContext()
+    {
+        //Instantiate both the windows
+        Form form1 = new Form1();
+        Form form2 = new Form2();
+
+        // When any window closes, remove it from the context
+        form1.FormClosed += (s, e) => ExitThreadIfNoForms();
+        form2.FormClosed += (s, e) => ExitThreadIfNoForms();
+
+        //Show both the windows
+        form1.Show();
+        form2.Show();
+    }
+
+    //Stop program if both windows close
+    private void ExitThreadIfNoForms()
+    {
+        if (Application.OpenForms.Count == 0)
+            ExitThread();
+    }
+}
+
+static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new MyContext());
+    }
+}
